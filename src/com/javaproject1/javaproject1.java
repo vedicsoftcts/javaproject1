01ffbb413ec7f0384f9494aca333cdff82cd87e5
Latif Vedic <latif@vedicsoftcts.com>
2020-01-07 16:48:55 +0530

    elciplsdkogh

---
+++ src/com/javaproject1/javaproject1.java
@@ -0,0 +1,10 @@
+package com.javaproject1;
+
+public class javaproject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java Project");
+	}
+
+}
